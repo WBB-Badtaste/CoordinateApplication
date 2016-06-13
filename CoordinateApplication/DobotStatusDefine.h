ba2c1b4290948ac38b46079e3e8faa2ac08b14f0
WBB-Badtaste <sos901012@gmail.com>
2016-06-13 14:10:14 +0800

    update

--- CoordinateApplication/DobotStatusDefine.h
+++ CoordinateApplication/DobotStatusDefine.h
@@ -39,13 +39,13 @@ const unsigned PALLET_SUBSYS_MASK		= (0x00000001 << DOBOT_SUBSYS_SHIFT);
 */
 
 const unsigned DOBOT_ERR_COORD_MASK							= (DOBOT_ERROR_MASK | COORDINATE_SUBSYS_MASK);
-const unsigned DOBOT_ERR_COORD_SET_MULTI_BASE				= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 0));
-const unsigned DOBOT_ERR_COORD_DEL_ID_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 1));
-const unsigned DOBOT_ERR_COORD_CONVERT_ORIGIN_NONEXISTENT	= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 2));
-const unsigned DOBOT_ERR_COORD_CONVERT_TARGET_NONEXISTENT	= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 3));
-const unsigned DOBOT_ERR_COORD_ERGODIC_EMPTY				= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 4));
-const unsigned DOBOT_ERR_COORD_GET_ID_NOTE					= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 5));
-const unsigned DOBOT_ERR_COORD_GET_ID						= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 6));
+const unsigned DOBOT_ERR_COORD_SET_MULTI_BASE				= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 0)); //using points witch made sense in difference coordinate to setup another coordinate
+const unsigned DOBOT_ERR_COORD_DEL_ID_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 1)); //the ID witch being gone to delete is not existent
+const unsigned DOBOT_ERR_COORD_CONVERT_ORIGIN_NONEXISTENT	= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 2)); //the origin coordinate used for converting is not existent
+const unsigned DOBOT_ERR_COORD_CONVERT_TARGET_NONEXISTENT	= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 3)); //the target coordinate used for converting is not existent
+const unsigned DOBOT_ERR_COORD_ERGODIC_EMPTY				= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 4)); //there is not any coordinate for ergodicing
+const unsigned DOBOT_ERR_COORD_GET_ID_NOTE					= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 5)); //the Index for coordinate in the vector is not existent
+const unsigned DOBOT_ERR_COORD_GET_ID						= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MASK + 6)); //the Index for coordinate in the vector is not existent
 
 /*
 @brief: define the coordinate subsystem success status
@@ -54,13 +54,13 @@ const unsigned DOBOT_ERR_COORD_GET_ID						= ((DOBOT_STATUS)(DOBOT_ERR_COORD_MAS
 */
 
 const unsigned DOBOT_SUC_COORD_MASK			= (DOBOT_SUCCESS_MASK | COORDINATE_SUBSYS_MASK);
-const unsigned DOBOT_SUC_COORD_CREATE		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 0));
-const unsigned DOBOT_SUC_COORD_MODIFY		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 1));
-const unsigned DOBOT_SUC_COORD_DEL			= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 2));
-const unsigned DOBOT_SUC_COORD_CONVERT		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 3));
-const unsigned DOBOT_SUC_COORD_ERGODIC		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 4));
-const unsigned DOBOT_SUC_COORD_GET_ID_NOTE	= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 5));
-const unsigned DOBOT_SUC_COORD_GET_ID		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 6));
+const unsigned DOBOT_SUC_COORD_CREATE		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 0)); //success to create a coordinate
+const unsigned DOBOT_SUC_COORD_MODIFY		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 1)); //success to modify a coordinate
+const unsigned DOBOT_SUC_COORD_DEL			= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 2)); //success to delete a coordinate
+const unsigned DOBOT_SUC_COORD_CONVERT		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 3)); //success to convert two DOBOT_POSITION
+const unsigned DOBOT_SUC_COORD_ERGODIC		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 4)); //success to get a coordinate dealing ergodic
+const unsigned DOBOT_SUC_COORD_GET_ID_NOTE	= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 5)); //success to get ID and note of a coordinate by index
+const unsigned DOBOT_SUC_COORD_GET_ID		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 6)); //success to get ID of a coordinate by index
 
 
 /*
@@ -70,8 +70,8 @@ const unsigned DOBOT_SUC_COORD_GET_ID		= ((DOBOT_STATUS)(DOBOT_SUC_COORD_MASK + 
 */
 
 const unsigned DOBOT_WAR_COORD_MASK			= (DOBOT_WARN_MASK | COORDINATE_SUBSYS_MASK);
-const unsigned DOBOT_WAR_COORD_CONVERT_SAME = ((DOBOT_STATUS)(DOBOT_WAR_COORD_MASK + 0));
-const unsigned DOBOT_WAR_COORD_ERGODIC_END	= ((DOBOT_STATUS)(DOBOT_WAR_COORD_MASK + 1));
+const unsigned DOBOT_WAR_COORD_CONVERT_SAME = ((DOBOT_STATUS)(DOBOT_WAR_COORD_MASK + 0)); //the coordinates used for converting are the same
+const unsigned DOBOT_WAR_COORD_ERGODIC_END	= ((DOBOT_STATUS)(DOBOT_WAR_COORD_MASK + 1)); //get a end signal of coordinate vector dealing ergodic
 
 
 /*
@@ -81,18 +81,18 @@ const unsigned DOBOT_WAR_COORD_ERGODIC_END	= ((DOBOT_STATUS)(DOBOT_WAR_COORD_MAS
 */
 
 const unsigned DOBOT_ERR_PALLET_MASK							= (DOBOT_ERROR_MASK | PALLET_SUBSYS_MASK);
-const unsigned DOBOT_ERR_PALLET_SET_NONLENGHT					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 0));
-const unsigned DOBOT_ERR_PALLET_SET_ZONE_LESS					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 1));
-const unsigned DOBOT_ERR_PALLET_SET_CREATE_COORD				= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 2));
-const unsigned DOBOT_ERR_PALLET_SET_ORIGIN_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 3));
-const unsigned DOBOT_ERR_PALLET_DEL_NONEXISTENT					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 4));
-const unsigned DOBOT_ERR_PALLET_GETCELL_PALLET_NONEXISTENT		= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 5));
-const unsigned DOBOT_ERR_PALLET_GETCELL_ZONE_OVER				= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 6));
-const unsigned DOBOT_ERR_PALLET_GETCELL_CONVERT					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 7));
-const unsigned DOBOT_ERR_PALLET_GETPALLET_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 8));
-const unsigned DOBOT_ERR_PALLET_GETPALLETID_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 9));
-const unsigned DOBOT_ERR_PALLET_GETPALLETID_BYINDEX_NONEXISTENT = ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 10));
-const unsigned DOBOT_ERR_PALLET_ERGODIC_EMPTY					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 11));
+const unsigned DOBOT_ERR_PALLET_SET_NONLENGHT					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 0));  //x lenght or y lenght is 0
+const unsigned DOBOT_ERR_PALLET_SET_ZONE_LESS					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 1));  //x zone or y zone less than 2
+const unsigned DOBOT_ERR_PALLET_SET_CREATE_COORD				= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 2));  //the coordinate can not be created for a new pallet
+const unsigned DOBOT_ERR_PALLET_SET_ORIGIN_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 3));  //the coordinate of the points witch used for setting up pallet is not existent
+const unsigned DOBOT_ERR_PALLET_DEL_NONEXISTENT					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 4));  //the ID of pallet witch being gone to delete is not existent
+const unsigned DOBOT_ERR_PALLET_GETCELL_PALLET_NONEXISTENT		= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 5));  //the pallet is not existent dealing getting cell of pallet
+const unsigned DOBOT_ERR_PALLET_GETCELL_ZONE_OVER				= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 6));  //the x zone or y zone number of cell is bigger than the pallet size
+const unsigned DOBOT_ERR_PALLET_GETCELL_CONVERT					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 7));  //it can not convert the position of cell in target coordinate
+const unsigned DOBOT_ERR_PALLET_GETPALLET_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 8));  //the ID of pallet witch being got is not existent
+const unsigned DOBOT_ERR_PALLET_GETPALLETID_NONEXISTENT			= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 9));  //the index of pallet witch being got is not existent
+const unsigned DOBOT_ERR_PALLET_GETPALLETID_BYINDEX_NONEXISTENT = ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 10)); //the index of pallet witch being got is not existent
+const unsigned DOBOT_ERR_PALLET_ERGODIC_EMPTY					= ((DOBOT_STATUS)(DOBOT_ERR_PALLET_MASK + 11)); //there is not any pallet for ergodicing
 
 
 
@@ -103,14 +103,14 @@ const unsigned DOBOT_ERR_PALLET_ERGODIC_EMPTY					= ((DOBOT_STATUS)(DOBOT_ERR_PA
 */
 
 const unsigned DOBOT_SUC_PALLET_MASK				= (DOBOT_SUCCESS_MASK | PALLET_SUBSYS_MASK);
-const unsigned DOBOT_SUC_PALLET_MODIFY				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 0));
-const unsigned DOBOT_SUC_PALLET_CREATE				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 1));
-const unsigned DOBOT_SUC_PALLET_DEL					= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 2));
-const unsigned DOBOT_SUC_PALLET_GETCELL				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 3));
-const unsigned DOBOT_SUC_PALLET_GETPALLET			= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 4));
-const unsigned DOBOT_SUC_PALLET_GETPALLETID			= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 5));
-const unsigned DOBOT_SUC_PALLET_GETPALLET_BYINDEX	= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 6));
-const unsigned DOBOT_SUC_PALLET_ERGODIC				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 7));
+const unsigned DOBOT_SUC_PALLET_MODIFY				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 0)); //success to modify the pallet
+const unsigned DOBOT_SUC_PALLET_CREATE				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 1)); //success to create the pallet
+const unsigned DOBOT_SUC_PALLET_DEL					= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 2)); //success to delete the pallet
+const unsigned DOBOT_SUC_PALLET_GETCELL				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 3)); //success to get cell of the pallet
+const unsigned DOBOT_SUC_PALLET_GETPALLET			= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 4)); //success to get the pallet
+const unsigned DOBOT_SUC_PALLET_GETPALLETID			= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 5)); //success to get id of the pallet
+const unsigned DOBOT_SUC_PALLET_GETPALLET_BYINDEX	= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 6)); //success to get the pallet by index
+const unsigned DOBOT_SUC_PALLET_ERGODIC				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MASK + 7)); //success to get a pallet dealing ergodic
 
 /*
 @brief: define the pallet subsystem warn status
@@ -119,4 +119,4 @@ const unsigned DOBOT_SUC_PALLET_ERGODIC				= ((DOBOT_STATUS)(DOBOT_SUC_PALLET_MA
 */
 
 const unsigned DOBOT_WAR_PALLET_MASK		= (DOBOT_WARN_MASK | PALLET_SUBSYS_MASK);
-const unsigned DOBOT_WAR_PALLET_ERGODIC_END = ((DOBOT_STATUS)(DOBOT_WAR_PALLET_MASK + 0));
+const unsigned DOBOT_WAR_PALLET_ERGODIC_END = ((DOBOT_STATUS)(DOBOT_WAR_PALLET_MASK + 0)); //get a end signal of pallet vector dealing ergodic
